ac178be1965dc94857bd4608a93149e3dcd2b2bf
Pradeepkumar064 <114431418+Pradeepkumar064@users.noreply.github.com>
2022-10-06 20:55:58 +0530

    Update Percentage calculator.py

--- Percentage calculator.py
+++ Percentage calculator.py
@@ -1,12 +1,12 @@
 def percentage(interest_rate, principle):
     i = 1
-    while i <= 500:
+    while i <= 365:
         interest = (interest_rate * principle)/100
         principle = interest + principle
         print(principle)
         i += 1
 
-# Taking input from users for bath interest rate and principle
+# Taking input from users for both interest rate and principle
 print("Type the expected Interest rate:")
 Int_rate = float(input())
 print("Enter the principle amount:")
